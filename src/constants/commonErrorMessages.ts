6ea0f5de93c639d0bdd061019434598d10da2489
Yadagiri Talada <yadagiri_talada@epam.com>
2024-06-30 16:11:01 +0530

    Updated updateProfile API

---
+++ src/constants/commonErrorMessages.ts
@@ -0,0 +1,4 @@
+export const COMMON_ERRORS = {
+    USER_FETCHING_ERROR: 'An error occurred while fetching details, Please try again !',
+    USER_UPDATING_ERROR: 'An error occurred while updating details, Please try again !'
+};
